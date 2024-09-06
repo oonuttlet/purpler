8ce551c6672e38422e7d7d8461c6ae01c1f83c1c
oonuttlet <77589230+oonuttlet@users.noreply.github.com>
2024-09-05 23:23:40 -0400

    documented get_sensors_data()

--- R/get_sensors_data.R
+++ R/get_sensors_data.R
@@ -2,12 +2,25 @@ require(httr2)
 require(jsonlite)
 require(sf)
 
+#' Retrieves a list with the latest data of multiple sensors matching the provided parameters.
+#'
+#' @param aoi Optional; an sf object defining an area of interest
+#' @param fields A character vector of fields specified in the [PurpleAir API documentation](https://api.purpleair.com/#api-sensors-get-sensors-data)
+#' @param api_key If not set with purpleair_api_key()
+#' @param ...
+#'
+#' @return A POINT sf object
+#' @export
+#'
+#' @examples
+#' nc <- st_read(system.file("shape/nc.shp", package="sf"))
+#' get_sensors_data(nc, c("name", "pm2.5", "humidity"))
 get_sensors_data <- function(aoi = NULL, fields, api_key = Sys.getenv("PURPLEAIR_API_KEY"), ...){
   if (is.null(aoi)){
     qry <- list(fields = append(fields, values = c("latitude", "longitude")),
                 api_key = api_key)
   }
-  else if (!inherits(aoi, sf)){
+  else if (!inherits(aoi, "sf")){
     stop("'aoi' must be an sf object.")
   }
   else {
